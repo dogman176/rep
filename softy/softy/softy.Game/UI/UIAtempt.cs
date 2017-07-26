7774ffec7242150ad1da0c24985f4f1f523aa449
SFort <SFort@sfort.tech>
2017-07-26 16:46:54 +0200

    Buttons

---
+++ softy/softy/softy.Game/UI/UIAtempt.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using SiliconStudio.Xenko.Engine;
+using SiliconStudio.Xenko.UI;
+using SiliconStudio.Xenko.UI.Controls;
+
+namespace softy
+{
+    class _UIAtempt : AsyncScript
+    {
+        public override async Task Execute()
+        {
+            await Script.NextFrame();
+            UIComponent Dingus = Entity.Get<UIComponent>();
+
+            Dingus.Page.RootElement.FindVisualChildOfType<Button>("2Dbutt").Click += delegate
+            {
+                var myScene = Content.Load<Scene>("Scene2D");
+                SceneSystem.SceneInstance.RootScene = myScene;
+            };
+            Dingus.Page.RootElement.FindVisualChildOfType<Button>("3Dbutt").Click += delegate
+            {
+                var myScene = Content.Load<Scene>("Scene3D");
+                SceneSystem.SceneInstance.RootScene = myScene;
+            };
+        }
+    }
+}
